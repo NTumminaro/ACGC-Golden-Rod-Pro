83242bb1bbde5a56443752575482e6576f357a4e
Tumminaro <nicholas.tumminaro@ymcadallas.org>
2023-03-29 12:33:29 -0500

    possible small tweaks to file structure

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+
+// Vite seems to be like our server js? its doing something on the backend thats running the localhost to port 5173? Neat though, not sure if we want to replace it with what we know
