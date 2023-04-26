de7eb8afa6a3722c6a475fa0e78d2d63b9a010e5
gabess3 <95196142+gabess3@users.noreply.github.com>
2023-04-26 16:46:01 -0500

    updated month buttons and added function to filter fish based on the month they click

--- src/components/monthButtons.jsx
+++ src/components/monthButtons.jsx
@@ -1,10 +1,30 @@
-import { Button } from "@mui/material";
+import { Button, ButtonGroup } from "@mui/material";
 
 // This will be able to take in the fish from the json, and create a button 
 // for each month that still appears in the list of fish (i.e. not in the caught fish array)
-function MonthButtons({fish}) {
+function MonthButtons({ fishData, caughtFish }) {
+
+    function filterFish(event) {
+        fishData.filter((fish) => fish.month.includes(event.target.value))
+    }
+
+
     return (
-        <Button>{fish.month}</Button>
+
+        <ButtonGroup>
+            <Button onClick={filterFish}>01</Button>
+            <Button onClick={filterFish}>02</Button>
+            <Button onClick={filterFish}>03</Button>
+            <Button onClick={filterFish}>04</Button>
+            <Button onClick={filterFish}>05</Button>
+            <Button onClick={filterFish}>06</Button>
+            <Button onClick={filterFish}>07</Button>
+            <Button onClick={filterFish}>08</Button>
+            <Button onClick={filterFish}>09</Button>
+            <Button onClick={filterFish}>10</Button>
+            <Button onClick={filterFish}>11</Button>
+            <Button onClick={filterFish}>12</Button>
+        </ButtonGroup>
     )
 }
 
